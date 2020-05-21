9d1fa22d2fb99101c85c278f0b0b66e1cbdb688a
John Yu <goldtruffle2015@hotmail.com>
2020-05-21 13:33:28 -0600

    Added functionality to nav button

---
+++ app.js
@@ -0,0 +1,11 @@
+// -- Attributes -- //
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const nav_li = document.querySelectorAll(".nav-links li")
+
+// -- Functions -- //
+
+// -- Code Starts Here -- //
+burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+})
